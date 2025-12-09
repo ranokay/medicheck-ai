ea9f298dd650caa4b41466f7a9d2339beacb14fd
ranokay <github@ranokay.com>
2025-12-09 13:46:41 +0200

    initial commit

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,40 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import ReactDOM from "react-dom/client";
+import Loader from "./components/loader";
+import { routeTree } from "./routeTree.gen";
+
+import { ConvexReactClient } from "convex/react";
+import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
+import { authClient } from "@/lib/auth-client";
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
+const router = createRouter({
+	routeTree,
+	defaultPreload: "intent",
+	defaultPendingComponent: () => <Loader />,
+	context: {},
+	Wrap: function WrapComponent({ children }: { children: React.ReactNode }) {
+		return (
+			<ConvexBetterAuthProvider client={convex} authClient={authClient}>
+				{children}
+			</ConvexBetterAuthProvider>
+		);
+	},
+});
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+const rootElement = document.getElementById("app");
+
+if (!rootElement) {
+	throw new Error("Root element not found");
+}
+
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(<RouterProvider router={router} />);
+}
